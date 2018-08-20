60ba47999f1ae0efda9b3d4e4a2e2a8a4a0758b5
subayadhav <suba10499@gmail.com>
2018-08-20 22:25:28 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,15 @@
+public class alphabet
+{
+  public static void main(String args[])
+{
+  char c='*';
+  if((c>='a'&&c<='z')&&(c>='A'&&C<='Z'))
+  {
+    System.out.println("the given letter is alphabet");
+  }
+  else
+  {
+    System.out.println("the given letter is not a alphabet"):
+  }
+  }
+  }
